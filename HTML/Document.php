721844b9093850476ed2a50ed22d0e8f4136fc98
Lcf.vs <lcf.vs@hotmail.com>
2015-03-23 08:55:34 +0100

    - testing code

--- HTML/Document.php
+++ HTML/Document.php
@@ -141,36 +141,3 @@ class DOM_HTML_Document extends DOM_Document
         }
     }
 }
-
-$view = new DOM_HTML_Document(true);
-$view->title = 'DOM_HTML_Document';
-
-$body = $view->body;
-
-$body->append([
-    'tag' => 'h2',
-    'data' => '... world!',
-    'attributes' => [
-        'class' => 'class-name',
-        'data-test' => 'value'
-    ]
-]);
-
-$body->insert([
-    'tag' => 'h1',
-    'data' => 'Hello ...'
-], 'h2');
-
-$body->append([
-    'tag' => 'input',
-    'attributes' => [
-        'type' => 'checkbox',
-        'checked' => 'checked',
-        'autocomplete' => true
-    ]
-]);
-
-$body->append([
-    'tag' => 'code',
-    'data' => (string) $body->select('$body>input[type="checkbox"]:autocomplete:checked')
-]);
